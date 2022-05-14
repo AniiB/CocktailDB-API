994ed72a475d318e8c6b56457a2f4584f22e08d2
Anirban <ani.b@tuta.io>
2022-05-14 18:52:44 +0530

    Minor presentation changes

--- js/main.js
+++ js/main.js
@@ -2,42 +2,35 @@
 //https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita
 
 //Check for button click event
-document.querySelector('button').addEventListener('click', getCocktail)
+document.querySelector('button').addEventListener('click', fetchCocktail)
 
-function getCocktail() {
+function fetchCocktail() {
+
     let ingredientList = document.querySelector('#ingredients')
     ingredientList.innerText= ''
-    
+    //Take user input value from the DOM
+    let drink = document.querySelector('input').value
 
-//Take user input value from the DOM
-let drink = document.querySelector('input').value
+    //Fetch API
+    fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) //parse respone to JSON
+    .then(data => {
+        console.log(data)
+        //Select the relevant drink object
+        let drinkObj = data.drinks[0]
+        //Display the name
+        document.querySelector('#cocktailName').innerText = drinkObj.strDrink
+        //Display the Image
+        document.querySelector('img').src = drinkObj.strDrinkThumb
 
-//Fetch API
-fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
-.then(res => res.json()) //parse respone to JSON
-.then(data => {
-    console.log(data)
-    //Select the relevant drink object
-    let drinkObj = data.drinks[0]
+        //Looping through every property to check if it is a valid ingredient. Display it if true
+        for(let key in drinkObj) {
+            if(key.includes('strIngredient') && drinkObj[key] !== null ) ingredientList.innerHTML += `<li>${drinkObj[key]}</li>` 
+        }
 
-    //Display the name
-    document.querySelector('#cocktailName').innerText = drinkObj.strDrink
+        //Display Instructions
+        document.querySelector('#instructionsEnglish').innerText = drinkObj.strInstructions 
 
-    //Display the Image
-    document.querySelector('img').src = drinkObj.strDrinkThumb
-
-    //Display the ingredients
- 
-    for(let key in drinkObj) {
-        if(key.includes('strIngredient') && drinkObj[key] !== null ) ingredientList.innerHTML += `<li>${drinkObj[key]}</li>` 
-    }
-
-    //Display Instructions
-    document.querySelector('#instructionsEnglish').innerText = drinkObj.strInstructions 
-
-})
-.catch(error => console.log(`error, ${error}`))
+    })
+    .catch(error => console.log(`error, ${error}`))
 }
-
-
-
