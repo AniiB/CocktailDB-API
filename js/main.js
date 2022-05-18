1fde17dd8320017219c5d948e14478f90831f5ba
Anirban <ani.b@tuta.io>
2022-05-18 10:19:52 +0530

    Drinks relevant to search query now appear in an automatic carousel/slideshow

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-//API link
-//https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita
-
-//Check for button click event
-document.querySelector('button').addEventListener('click', fetchCocktail)
+let btn = document.querySelector('button')
+btn.addEventListener('click', fetchCocktail)
 
 function fetchCocktail() {
-    
     let drink = document.querySelector('input').value.toLowerCase()
 
     fetch(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
     .then(res => res.json()) //parse respone to JSON
     .then(data => {
-        if (drink === data.drinks[0].strDrink.split(' ')[0].toLowerCase()) displayDetails(data) 
+        if (drink === data.drinks[0].strDrink.split(' ')[0].toLowerCase()) {
+            displayDetails(data) 
+        }
         else {
             resetDOM()
             document.querySelector('.displayError').innerText = 'No drink found :(' 
@@ -21,31 +19,43 @@ function fetchCocktail() {
 }
 
 function resetDOM() {
-    document.querySelector('section + section').classList.add('hidden')
     document.querySelector('.displayError').innerText = ''
-    const ingredients = document.querySelector('#ingredients')
-    ingredients.innerText = ''
-
-    
+    document.querySelector('#ingredients').innerText = ''
 }
-
+      
 function displayDetails(data) {
-    resetDOM()
     const drinkName = document.querySelector('#cocktailName')
     const drinkImg = document.querySelector('img')
     const ingredients = document.querySelector('#ingredients')
     const instructions = document.querySelector('#instructions')
+    const instrHeading = document.querySelector('.instructionsHeading')
+    const ingreHeading = document.querySelector('.ingredientsHeading')
+    let i = 0
+    let drinkArray = data.drinks
 
-    let drinkObj = data.drinks[0]
+    slideShow()
 
-    document.querySelector('section + section').classList.remove('hidden')
+    function slideShow() {
+        drinkName.innerText = drinkArray[i].strDrink
+        drinkImg.src = drinkArray[i].strDrinkThumb
+        
+        ingredients.innerText = ''
+        ingreHeading.innerText = `Ingredients`
+        for (let key in drinkArray[i]) {
+            if(key.includes('strIngredient') && (drinkArray[i][key] !== null && drinkArray[i][key] !== '')) {
+                ingredients.innerHTML += `<li>${drinkArray[i][key]}</li>`
+            }
+        }
+        instrHeading.innerText = `Instructions`
+        instructions.innerText = drinkArray[i].strInstructions
 
-    drinkName.innerText = drinkObj.strDrink
-    drinkImg.src = drinkObj.strDrinkThumb
-    instructions.innerText = drinkObj.strInstructions 
-
-    for(let key in drinkObj) {
-        if(key.includes('strIngredient') && drinkObj[key] !== null ) ingredients.innerHTML += `<li>${drinkObj[key]}</li>` 
+        if(i < drinkArray.length-1) i++
+        else i=0
+    
+       timeO = setTimeout(slideShow, 3000)
+    
+       btn.addEventListener('click', () => {
+        clearTimeout(timeO)
+        })
     }
 }
-        
